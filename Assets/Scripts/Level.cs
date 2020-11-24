e528ce32fe237b54476600ca127797814bc56672
Chris Twaddell <ct9233@gmail.com>
2020-11-23 18:09:59 -0800

    Count for Breakable Blocks

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    // Serialized for debugging
+    [SerializeField] int breakableBlocks;
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+}
