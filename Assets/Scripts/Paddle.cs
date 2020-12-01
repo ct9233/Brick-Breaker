81143b4c43b2d21f9fa4d5d03e7df1029fa91e3b
Chris Twaddell <ct9233@gmail.com>
2020-11-30 18:09:36 -0800

    Autoplay Setup for Playtesting

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -8,18 +8,34 @@ public class Paddle : MonoBehaviour
     [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
 
+    // Cached references
+    GameSession theGameSession;
+    Ball theBall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
         Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
-        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
         transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
     }
 }
